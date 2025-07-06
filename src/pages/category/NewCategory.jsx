6becec329a70fbcd06764b16dc1ed5fd26f84db7
Abdul Ashik <syncthrive.meh@gmail.com>
2025-07-06 16:10:02 +0530

    Day 03 of commit changes (Add category completion)

--- src/pages/category/NewCategory.jsx
+++ src/pages/category/NewCategory.jsx
@@ -1,14 +1,82 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "../../utils/axiosInstance";
+import addCategoryValidator from "../../validators/addCategoryValidation";
+
+const initialFormData = {
+  title: "",
+  desc: "",
+};
+
+const initialFormError = {
+  title: "",
+};
 
 const NewCategory = () => {
+  const [formData, setFormData] = useState(initialFormData);
+  const [formError, setFormError] = useState(initialFormError);
+  const [loading, setLoading] = useState(false);
+
   const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const errors = addCategoryValidator({
+      title: formData.title,
+    });
+
+    if (errors.title) {
+      setFormError(errors);
+    } else {
+      try {
+        setLoading(true);
+
+        const requestBody = {
+          title: formData.title,
+          desc: formData.desc,
+        };
+
+        const response = await axios.post(
+          "/category/add-category",
+          requestBody
+        );
+        const data = response.data;
+
+        toast.success(data.message, {
+          position: "top-right",
+          autoClose: 2000,
+        });
+
+        setFormData(initialFormData);
+        setFormError(initialFormError);
+        setLoading(false);
+
+        navigate("/categories");
+      } catch (error) {
+        setLoading(false);
+        const response = error.response;
+        const data = response.data;
+
+        toast.error(data.message, {
+          position: "top-right",
+          autoClose: 2000,
+        });
+      }
+    }
+  };
+
   return (
     <div>
       <button className="button button-block" onClick={() => navigate(-1)}>
         Go Back
       </button>
       <div className="form-container">
-        <form className="inner-container">
+        <form className="inner-container" onSubmit={handleSubmit}>
           <h2 className="form-title">New Category</h2>
           <div className="form-group">
             <label>Title</label>
@@ -16,8 +84,11 @@ const NewCategory = () => {
               className="form-control"
               type="text"
               name="title"
+              value={formData.title}
+              onChange={handleChange}
               placeholder="enter title"
             />
+            {formError.title && <p className="error">{formError.title}</p>}
           </div>
           <div className="form-group">
             <label>Description</label>
@@ -25,11 +96,17 @@ const NewCategory = () => {
               className="form-control"
               type="text"
               name="desc"
+              value={formData.desc}
+              onChange={handleChange}
               placeholder="enter description"
             />
           </div>
           <div className="form-group">
-            <input className="button" type="submit" value={"Add"} />
+            <input
+              className="button"
+              type="submit"
+              value={`${loading ? "Adding..." : "Add"}`}
+            />
           </div>
         </form>
       </div>
